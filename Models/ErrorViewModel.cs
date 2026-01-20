039cbb44b882ac88146740b36812e9b1859e4db5
Enes Yılmaz <enesyilmaz.ey3@gmail.com>
2026-01-20 16:52:20 +0300

    ASP.NET Core Identity projeye eklendi.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
