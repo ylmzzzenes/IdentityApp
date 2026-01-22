3c15d454890e7cc6f8225d80cda716ff5c535a09
Enes Yılmaz <enesyilmaz.ey3@gmail.com>
2026-01-23 00:41:04 +0300

    ASP.NET Identity veritabanı ve migration'lar eklendi

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<IdentityContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityApp"));
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
 var app = builder.Build();
 app.MapControllerRoute(
     name: "default",
