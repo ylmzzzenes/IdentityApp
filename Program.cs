59a5bb5fa7fd1fa9fe25408d7c90ba3a62d98a75
Enes Yılmaz <enesyilmaz.ey3@gmail.com>
2026-01-23 03:30:02 +0300

    IdentityOptions ayarları eklendi

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ builder.Services.AddDbContext<IdentityContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityApp"));
 });
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.AllowedForNewUsers = true;
+
+    options.User.RequireUniqueEmail = true;
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+});
 var app = builder.Build();
 app.MapControllerRoute(
     name: "default",
